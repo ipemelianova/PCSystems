41dc17842a1a388c28b619476860db641ff343fc
ipemelianova <107342108+ipemelianova@users.noreply.github.com>
2022-06-24 09:07:30 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale = 1">
+<title>Website</title>
+<link rel="stylesheet" href="products.css">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+ 
+<body>
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <h5 class="my-0 mr-md-auto fon-weight0normal">Товары</h5>
+     <a class="btn-outline-primary" href="index.html">Главная страница</a>
+    </div>
+  <div class="container">
+    Товары, которые мы продаём
+  </div>
+  
+</body>
