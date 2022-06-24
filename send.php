85421c3d18b86f846f876211432ffd454989663c
ipemelianova <107342108+ipemelianova@users.noreply.github.com>
2022-06-24 15:20:56 +1000

    Update send.php

--- send.php
+++ send.php
@@ -1,1 +1,52 @@
-
+<?
+ 
+if(isset($_POST["name"]))
+{
+        if(isset($_POST["name"]))
+        {
+                $name = $_POST["name"];
+        }
+        if(isset($_POST["phone"]))
+        {
+                $phone= $_POST["phone"];
+        }
+        if(isset($_POST["body"]))
+        {
+                $body = $_POST["body"];
+        }
+ 
+        if($name=="" or $phone=="" or $body=="")
+        { // Проверяем на заполненность всех полей.
+                echo "Пожалуйста, заполните все поля";
+        }
+        else
+        {
+                $ip=$_SERVER["REMOTE_ADDR"]; // Вычисляем ip пользователя
+                $brose=$_SERVER["HTTP_USER_AGENT"]; // Вычисляем браузер пользователя
+                $to = "alexkowalchuck@gmail.com"; // Ваш email адрес
+                $subject = "Сообщение c сайта a-devel.com"; // тема письма
+                $headers .= "Content-Type: text/html;
+                ";
+                $headers .= "Отправитель: Посетитель сайта"; // Отправитель письма
+                $message = "
+                Имя: $name<br>
+                Телефоны: $phone<br>
+                Текст: $body<br><br>
+ 
+                IP отправителя: $ip<br>
+                Браузер отправителя: $brose<br>
+                ";
+ 
+                $send = mail($to, $subject, $message, $headers);
+                if ($send == "true")
+                {
+                        echo "Ваше сообщение отправлено. Мы ответим вам в ближайшее время.";
+                }
+                else
+                {
+                        echo "Не удалось отправить, попробуйте снова!";
+                }
+        }
+}
+ 
+?>
